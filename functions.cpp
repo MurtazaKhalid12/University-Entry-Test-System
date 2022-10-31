73f497a97a4c569988e00adb52bd4e2ab62101a0
bsce22030 <bsce22030@itu.edu.pk>
2022-11-01 00:53:16 +0500

    Made some files

---
+++ functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ITU on 01/11/2022.
+//
+
+#include "functions.h"
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "functions.h"
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"The coding begins brothers and sisters and.... lets just leave it at that";
+    return 0;
+}
